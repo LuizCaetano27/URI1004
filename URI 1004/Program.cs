d4daa23d56d47478c2a3a39842c179131478dbbf
luiz.caetano <luiz.caetano@MMQ003477>
2023-09-21 16:47:44 -0300

    Adicionar arquivos de projeto.

---
+++ URI 1004/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Valor1, Valor2, PROD;
+
+            Valor1 = int.Parse(Console.ReadLine());
+            Valor2 = int.Parse(Console.ReadLine());
+
+            PROD = Valor1 * Valor2;
+
+            Console.WriteLine("PROD = " + (PROD));
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
